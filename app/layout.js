b2e9a2ef51f9f4083a587579360470111ea34e90
AdrienW86 <adrien_weiss@outlook.fr>
2025-07-23 13:12:18 +0200

    mise n ligne

--- app/layout.js
+++ app/layout.js
@@ -9,27 +9,28 @@ const inter = Inter({ subsets: ['latin'] })
 export default function RootLayout({ children }) {
   return (
     <html lang="fr">
-      <head>
-        <link rel="icon" href="favicon.png" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Lobster&display=swap"
-          rel="stylesheet"
-        />
-      </head>
+     <head>
+  <link rel="icon" href="favicon.png" />
+  <link
+    href="https://fonts.googleapis.com/css2?family=Lobster&display=swap"
+    rel="stylesheet"
+  />
+  <Script
+    src="https://www.googletagmanager.com/gtag/js?id=AW-17050234458"
+    strategy="afterInteractive"
+  />
+  <Script id="google-analytics" strategy="afterInteractive">
+    {`
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'AW-17050234458');
+    `}
+  </Script>
+</head>
+
       <body className={inter.className}>
-        {/* Scripts Google Analytics (client-side only) */}
-        <Script
-          src="https://www.googletagmanager.com/gtag/js?id=AW-17050234458"
-          strategy="afterInteractive"
-        />
-        <Script id="google-analytics" strategy="afterInteractive">
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'AW-17050234458');
-          `}
-        </Script>
+       
         <Header /> 
           {children}
         <Footer /> 
